4e2ee9689d94fbbddf0bd1d9af7f59d0c0bb2ccc
Wild Dragon Reina <a112231198@gmail.com>
2022-02-03 11:30:07 +0800

    加入專案檔案。

---
+++ Dupetopia/Duper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dupetopia
+{
+    class Duper
+    {
+        public Duper(string worldFolderNum, string masterIslandNum, string[] islandNums)
+        {
+            string appdataPath = Path.GetDirectoryName(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            string gameWorldPath = @"LocalLow\PocketPair\Craftopia\Save\Worlds\";
+
+            _gameSavePath = Path.Combine(appdataPath, gameWorldPath, worldFolderNum);
+            _dupeSavePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "WorkSpace");
+
+            _masterIslandName = GetIslandFileName(masterIslandNum);
+            _targetSaves = new List<string>(islandNums);
+
+            MakeCopyFromMainSave();
+        }
+
+        private void MakeCopyFromMainSave()
+        {
+            string srcIslandPath = Path.Combine(_gameSavePath, _masterIslandName);
+            string masterIslandPath = Path.Combine(_dupeSavePath, _masterIslandName);
+
+            Directory.CreateDirectory(_dupeSavePath);
+
+            ReplaceIslandFile(srcIslandPath, masterIslandPath);
+
+            string subIslandPath;
+            foreach (var subIslandNum in _targetSaves)
+            {
+                subIslandPath = Path.Combine(_dupeSavePath, GetIslandFileName(subIslandNum));
+
+                ReplaceIslandFile(masterIslandPath, subIslandPath);
+                ReplaceIslandFile(subIslandPath, Path.Combine(_gameSavePath, GetIslandFileName(subIslandNum)));
+            }
+        }
+
+        private string GetIslandFileName(string saveIslandNum)
+        {
+            return "Island" + saveIslandNum + ".ocs";
+        }
+
+        private void ReplaceIslandFile(string srcIslandPath, string destIslandPath)
+        {
+            if (File.Exists(destIslandPath))
+            {
+                File.Delete(destIslandPath);
+            }
+
+            File.Copy(srcIslandPath, destIslandPath, true);
+        }
+
+        private string _gameSavePath;
+        private string _dupeSavePath;
+        private string _masterIslandName;
+        private List<string> _targetSaves;
+    }
+}
---
+++ Dupetopia/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Dupetopia.Properties;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Dupetopia
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadLastInput();
+        }
+
+        private void DoDupe()
+        {
+            if (string.IsNullOrEmpty(TB_WorldName.Text) 
+                || string.IsNullOrEmpty(TB_MasterIslandName.Text) 
+                || string.IsNullOrEmpty(TB_SubIslandName.Text))
+            {
+                TB_Info.Content = "所有欄位不可為空";
+
+                return;
+            }
+
+            TB_Info.Content = "運作正常";
+
+            string[] subIslands = TB_SubIslandName.Text.Split('|');
+            
+            new Duper(TB_WorldName.Text, TB_MasterIslandName.Text, subIslands);
+        }
+
+        private void ClickDrag(object sender, MouseButtonEventArgs e) => DragMove();
+
+        private void LoadLastInput()
+        {
+            TB_WorldName.Text        = Settings.Default.worldFolderName;
+            TB_MasterIslandName.Text = Settings.Default.masterIslandNum;
+            TB_SubIslandName.Text    = Settings.Default.subIslandNum;
+            CB_Topmost.IsChecked     = Settings.Default.topmost;
+        }
+
+        private void SaveLastInput()
+        {
+            Settings.Default.worldFolderName = TB_WorldName.Text;
+            Settings.Default.masterIslandNum = TB_MasterIslandName.Text;
+            Settings.Default.subIslandNum    = TB_SubIslandName.Text;
+            Settings.Default.topmost         = CB_Topmost.IsChecked ?? false;
+            Settings.Default.Save();
+        }
+
+        private void ClickDupe(object sender, RoutedEventArgs e) => DoDupe();
+        private void ClickMinimize(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void ClickClose(object sender, RoutedEventArgs e)
+        {
+            SaveLastInput();
+
+            Close();
+
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void Topmost_Checked(object sender, RoutedEventArgs e) => Topmost = true;
+
+        private void Topmost_Unchecked(object sender, RoutedEventArgs e) => Topmost = false;
+    }
+}
